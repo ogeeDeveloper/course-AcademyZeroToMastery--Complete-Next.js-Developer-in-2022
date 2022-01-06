0d268916b2ce3d095dd0f3ea6049981aaaaa4246
ogeeDeveloper <omarogrant202@hotmail.com>
2022-01-06 10:02:18 -0500

    Added get Static Path functionn to the ID page

--- src/pages/coffee-store/[id].js
+++ src/pages/coffee-store/[id].js
@@ -1,8 +1,32 @@
 // Hook to read route from the browser URL
 import {useRouter} from 'next/router'
 import Link from 'next/link'
+import CoffeeStoreData from '../../utils/coffeeData.json'
 
-const CoffeeStore = () => {
+export async function getStaticProps(staticProps){
+    const params = staticProps.params
+    // const data = CoffeeStoreData
+    return{
+        props:{
+            CoffeeStore: CoffeeStoreData.find(CoffeeStores=>{
+                // Returns the first id it gets from thhe dynnamic id in the URL
+                return CoffeeStores.id.toString() === params.id
+            }),
+        },
+            
+    }
+}
+
+export async function getStaticPaths(){
+    return {
+        paths:[
+            {params: {id:"0",id:"1"}}
+        ],
+        fallback: true
+    }
+}
+
+const CoffeeStore = (props) => {
     const router = useRouter() // This is a object that has a lot of values such as the 'id' from query
     const {id} = router.query
 
@@ -12,6 +36,8 @@ const CoffeeStore = () => {
             <Link href="/">
                 <a>Back to Home</a>
             </Link>
+            <p>{props.CoffeeStore.name}</p>
+            <p>{props.CoffeeStore.city}</p>
         </div>
     )
 }
