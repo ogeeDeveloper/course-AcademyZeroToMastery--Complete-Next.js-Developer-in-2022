e569e793c96790d947e7056bb007185a87f2a009
ogeeDeveloper <omarogrant202@hotmail.com>
2022-06-16 13:21:50 -0500

    Created the Serverless Function to retrieve the coffee stores basedx on the user location

---
+++ src/pages/api/getCoffeeStoreByLocation.js
@@ -0,0 +1,19 @@
+import {FetchCoffeeStore} from "../../lib/coffee-store"
+
+// API to retrieve the coffee stores
+export default getCoffeeStoreByLocation = async(req, res)=>{
+    // Configure latlongf and limit
+    try {
+        const {latLong, limit} = req.query // get latlong and query from query in URL
+
+        const response = await FetchCoffeeStore(latLong,30) 
+        res.status(200)
+        res.json(response)
+    } catch (error) {
+        console.log("Their was an error while fetching", eror)
+        res.status(500)
+        res.json({message: "🙄Opps, there was an error on the server", error})
+    }
+    
+    // return
+}
