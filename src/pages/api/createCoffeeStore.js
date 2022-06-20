3a51c226f99a586da57f7b9daf2927bff5a1b5f8
ogeeDeveloper <omarogrant202@hotmail.com>
2022-06-20 09:03:33 -0500

    Created the function to retrieve values from Airtable

--- src/pages/api/createCoffeeStore.js
+++ src/pages/api/createCoffeeStore.js
@@ -6,5 +6,31 @@ Airtable.configure({
     apiKey: process.env.AIRTABLE_API_KEY
 });
 
-// Thebase is used to call the API routes
+// The base is used to call the API routes
 const base = Airtable.base(process.env.AIRTABLE_BASE_ID)
+
+const table = base('cofee-stores')
+
+console.log({table})
+
+const createCoffeeStore = async(req, res)=>{
+    // Check to see if method is POST
+    if(req.method === "POST"){
+        // Find a Record
+        const findCoffeeStoreRecords =  await table.select({
+            filterByFormula: `id="0"`,
+        }).firstPage()
+
+        console.log({findCoffeeStoreRecords})
+
+        if(findCoffeeStoreRecords.length !==0){
+            res.json(findCoffeeStoreRecords)
+        }else{
+            //Create a record
+            res.json({message: "Create a record"})
+        }
+    }
+    // return res.json({message: "Hi there api configured succesfully"})
+}
+
+export default createCoffeeStore
