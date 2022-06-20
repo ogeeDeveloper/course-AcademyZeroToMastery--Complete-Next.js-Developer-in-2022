68eb79942a3f6fe54d2a6a780b39c0836bce60b9
ogeeDeveloper <omarogrant202@hotmail.com>
2022-06-20 11:59:11 -0500

    Implemented the create functionality to create a new coffee store to the Airtable

--- src/pages/api/createCoffeeStore.js
+++ src/pages/api/createCoffeeStore.js
@@ -19,7 +19,7 @@ const createCoffeeStore = async(req, res)=>{
         try{
             // Find a Record
             const findCoffeeStoreRecords =  await table.select({
-                filterByFormula: `id="0"`,
+                filterByFormula: `id="1"`,
             }).firstPage()
 
             console.log({findCoffeeStoreRecords})
@@ -35,12 +35,36 @@ const createCoffeeStore = async(req, res)=>{
                 res.json(records)
             }else{
                 //Create a record
-                res.json({message: "Create a record"})
+                const createRecords = await table.create([
+                    {
+                        fields:{
+                            ID: "1",
+                            name: "Uptown coffee funk",
+                            address: "1123 Tester",
+                            neighborhood: "top coupe",
+                            voting: 100,
+                            imageURL: "https://img.com"
+                        }
+                    }
+                ])
+                 // Transform data that is been retrieved from the API
+                 const records = createRecords.map(data=>{
+                    // Spread the fields inside the object soi we can get back all data in the fields object
+                    return{
+                        ...data.fields,
+                    }
+                })
+
+                res.json({message: "Create a record", record})
+
             }
         }catch(error){
-            console.log("Their was an error while fetching stores", error)
-            res.status(500).json({message: "Something went wrong, please try again later"})
+            console.log("Their was an error, please try again later", error)
+            res.status(500).json({message: "Something went wrong, please try again later", error})
         }
+    }else{
+        res.status(500).json({mesage: "The GET method is not allowed"});
+        console.log("You are using the wrong request method")
     }
     // return res.json({message: "Hi there api configured succesfully"})
 }
