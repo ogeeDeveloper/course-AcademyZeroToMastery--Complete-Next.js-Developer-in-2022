3bb6622fe15db0c7c3cc2180916e2551fc378622
ogeedeveloper <omarogrant202@hotmail.com>
2022-07-18 12:17:30 -0500

    Refactor the createCoffeeStore API

---
+++ src/lib/airtable.js
@@ -0,0 +1,35 @@
+//const Airtable = require('airtable')
+import Airtable from "airtable"
+
+Airtable.configure({
+    endpointUrl: 'https://api.airtable.com',
+    apiKey: process.env.AIRTABLE_API_KEY
+});
+
+// The base is used to call the API routes
+const base = Airtable.base(process.env.AIRTABLE_BASE_ID)
+
+const table = base('cofee-stores')
+
+// Get miified Record
+const getMinifiedRecord = record=>{
+    return{
+        recordId:record.id,
+        ...record.fields,
+    }
+}
+
+// Gets record from table
+const getRecords = records =>{
+    // Transform data that is been retrieved from the API
+    return records.map(record=>
+        // Spread the fields inside the object soi we can get back all data in the fields object
+        getMinifiedRecord(record)
+    )
+}
+
+
+export {
+    table,
+    getRecords
+}
--- src/pages/api/createCoffeeStore.js
+++ src/pages/api/createCoffeeStore.js
@@ -1,65 +1,48 @@
-//const Airtable = require('airtable')
-import Airtable from "airtable"
+import {table, getRecords } from "../../lib/airtable"
 
-Airtable.configure({
-    endpointUrl: 'https://api.airtable.com',
-    apiKey: process.env.AIRTABLE_API_KEY
-});
-
-// The base is used to call the API routes
-const base = Airtable.base(process.env.AIRTABLE_BASE_ID)
-
-const table = base('cofee-stores')
-
-console.log({table})
 
 const createCoffeeStore = async(req, res)=>{
-    // Check to see if method is POST
     if(req.method === "POST"){
         // Get the json to be added by access the body of the request
         const {ID, name, address, neighborhood, voting, imageURL} = req.body
         try{
-            // Find a Record
-            const findCoffeeStoreRecords =  await table.select({
-                filterByFormula: `ID=${ID}`,
-            }).firstPage()
+            if(ID){
+                // Find a Record
+                const findCoffeeStoreRecords =  await table.select({
+                    filterByFormula: `id="${ID}"`,
+                }).firstPage()
 
-            console.log({findCoffeeStoreRecords})
-
-            if(findCoffeeStoreRecords.length !==0){
-                // Transform data that is been retrieved from the API
-                    const records = findCoffeeStoreRecords.map(data=>{
-                        // Spread the fields inside the object soi we can get back all data in the fields object
-                        return{
-                            ...data.fields,
-                        }
-                    })
-                res.json(records)
+                if(findCoffeeStoreRecords.length !==0){
+                    const records = getRecords(findCoffeeStoreRecords)
+                    res.json(records)
+                }else{
+                    if(name){
+                        //Create a record
+                        const createRecords = await table.create([
+                            {
+                                fields:{
+                                    // Since key and value is the same for IDS I can just use the variable itself that was destructred from the request of the body rather than "name: nameOfCoffeeStore"
+                                    ID,
+                                    name,
+                                    address,
+                                    neighborhood,
+                                    voting,
+                                    imageURL,
+                                },
+                             },
+                        ])
+                        // Transform data that is been retrieved from the API
+                        const records = getRecords(createRecords)
+                        res.json(201)
+                        res.json({message: "Created a record successfully", records})
+                    }else{
+                        res.json(400)
+                        res.json({ message: "Name is missing" });
+                    }
+                }
+  
             }else{
-                //Create a record
-                const createRecords = await table.create([
-                    {
-                        fields:{
-                            // Since key and value is the same I can just use the vatiable itself that was destructred from the request of the body rather than "name: nameOfCoffeeStore"
-                            ID,
-                            name,
-                            address,
-                            neighborhood,
-                            voting,
-                            imageURL,
-                        }
-                    }
-                ])
-                 // Transform data that is been retrieved from the API
-                 const records = createRecords.map(data=>{
-                    // Spread the fields inside the object soi we can get back all data in the fields object
-                    return{
-                        ...data.fields,
-                    }
-                })
-
-                res.json({message: "Created a record", record})
-
+                res.status(400).json({message: "Something went wrong, ID is missing", error})
             }
         }catch(error){
             console.log("Their was an error, please try again later", error)
@@ -69,7 +52,6 @@ const createCoffeeStore = async(req, res)=>{
         res.status(500).json({mesage: "The GET method is not allowed"});
         console.log("You are using the wrong request method")
     }
-    // return res.json({message: "Hi there api configured succesfully"})
 }
 
 export default createCoffeeStore
